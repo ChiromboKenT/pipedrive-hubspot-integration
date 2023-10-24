50f3f84d7e4776a3e172fef5a2ffde30b0cca065
Kenny Chirombo <chirombokenny@gmail.com>
2023-10-24 14:15:26 +0200

    fix/deployment pipeline changes

--- set_heroku_env.sh
+++ set_heroku_env.sh
@@ -21,4 +21,6 @@ heroku config:set PIPEDRIVE_URL=$PIPEDRIVE_URL --app $HEROKU_APP_NAME
 heroku config:set PIPEDRIVE_API_TOKEN=$PIPEDRIVE_API_TOKEN --app $HEROKU_APP_NAME
 heroku config:set NODE_ENV=$NODE_ENV --app $HEROKU_APP_NAME
 
+heroku stack:set container -a $HEROKU_APP_NAME
+
 echo "Environment variables set on Heroku app $HEROKU_APP_NAME."
